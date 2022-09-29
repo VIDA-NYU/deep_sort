796a495da40b09ab752058dbaa089679b1845bed
Bea Steers <bea.steers@gmail.com>
2022-09-29 10:39:06 -0400

    comprehensive conversions

--- deep_sort/util.py
+++ deep_sort/util.py
@@ -4,40 +4,79 @@
 def get_ndim(xs):
     return xs.shape[-1]//2
 
-def tlbr2tlwh(tlbr):
-    """(top left, bottom right) => (top left x, top left y, width, height)"""
-    ndim = get_ndim(tlbr)
-    tlbr[...,ndim:] -= tlbr[...,:ndim]
-    return tlbr
+# tlwh <-> tlbr
 
-def tlwh2tlbr(tlwh):
+def tlwh2tlbr(tlwh, ndim=None):
     """(top left x, top left y, width, height) => (top left, bottom right)"""
-    ndim = get_ndim(tlwh)
+    ndim = ndim or get_ndim(tlwh)
     tlwh[...,ndim:] += tlwh[...,:ndim]
     return tlwh
 
-def tlwh2xyah(tlwh):
-    """(top left x, top left y, width, height) => (center x, center y, width/height, height)"""
-    ndim = get_ndim(tlwh)
+
+def tlbr2tlwh(tlbr, ndim=None):
+    """(top left, bottom right) => (top left x, top left y, width, height)"""
+    ndim = ndim or get_ndim(tlbr)
+    tlbr[...,ndim:] -= tlbr[...,:ndim]
+    return tlbr
+
+# tlwh <-> xywh
+
+def tlwh2xywh(tlwh, ndim=None):
+    """(top left x, top left y, width, height) => (top left, bottom right)"""
+    ndim = ndim or get_ndim(tlwh)
     tlwh[...,:ndim] += tlwh[...,ndim:] / 2
-    tlwh[...,ndim] /= tlwh[...,ndim+1]
-    if ndim == 3:
-        tlwh[...,-1] /= tlwh[...,ndim+1]
     return tlwh
 
-def xyah2tlwh(xyah):
+def xywh2tlwh(xywh, ndim=None):
+    """(top left, bottom right) => (top left x, top left y, width, height)"""
+    ndim = ndim or get_ndim(xywh)
+    xywh[...,:ndim] -= xywh[...,ndim:] / 2
+    return xywh
+
+# tlwh <-> xyah
+
+def tlwh2xyah(tlwh, ndim=None):
+    """(top left x, top left y, width, height) => (center x, center y, width/height, height)"""
+    return xywh2xyah(tlwh2xywh(tlwh, ndim), ndim)
+
+def xyah2tlwh(xyah, ndim=None):
     """(center x, center y, width/height, height) => (top left x, top left y, width, height)"""
-    ndim = get_ndim(xyah)
+    return xywh2tlwh(xyah2xywh(xyah, ndim), ndim)
+
+# xywh <-> xyah
+
+def xywh2xyah(xywh, ndim=None):
+    """(top left x, top left y, width, height) => (center x, center y, width/height, height)"""
+    ndim = ndim or get_ndim(xywh)
+    xywh[...,ndim] /= xywh[...,ndim+1]
+    if ndim == 3:
+        xywh[...,-1] /= xywh[...,ndim+1]
+    return xywh
+
+def xyah2xywh(xyah, ndim=None):
+    """(center x, center y, width/height, height) => (top left x, top left y, width, height)"""
+    ndim = ndim or get_ndim(xyah)
     xyah[...,ndim] *= xyah[...,ndim+1]
     if ndim == 3:
         xyah[...,-1] *= xyah[...,ndim+1]
-    xyah[...,:ndim] -= xyah[...,ndim:] / 2
     return xyah
 
-def xyah2tlbr(xyah):
-    '''(center x, center y, width/height, height) => (top left, bottom right)'''
-    return tlwh2tlbr(xyah2tlwh(xyah))
+# xywh <-> tlbr
 
-def tlbr2xyah(xyah):
+def xywh2tlbr(xywh, ndim=None):
+    '''(center x, center y, width/height, height) => (top left, bottom right)'''
+    return tlwh2tlbr(xywh2tlwh(xywh, ndim), ndim)
+
+def tlbr2xwh(tlbr, ndim=None):
     '''(top left, bottom right) => (center x, center y, width/height, height)'''
-    return tlwh2xyah(tlbr2tlwh(xyah))
+    return tlwh2xywh(tlbr2tlwh(tlbr, ndim), ndim)
+
+# xyah <-> tlbr
+
+def xyah2tlbr(xyah, ndim=None):
+    '''(center x, center y, width/height, height) => (top left, bottom right)'''
+    return tlwh2tlbr(xyah2tlwh(xyah, ndim), ndim)
+
+def tlbr2xyah(xyah, ndim=None):
+    '''(top left, bottom right) => (center x, center y, width/height, height)'''
+    return tlwh2xyah(tlbr2tlwh(xyah, ndim), ndim)
